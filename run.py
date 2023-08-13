b8036c3ce3aeb4f7c4ae9bc5bfba132f09b2ad99
jamieroche1987 <>
2023-08-13 15:59:38 +0000

    Create the turns of the battleship

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 import random
-
 # Board for computer
-
 def computer_board():
     board = {
         '1A':' ', '2A':' ', '3A':' ', '4A':' ', '5A':' ',
@@ -11,12 +9,10 @@ def computer_board():
         '1E':' ', '2E':' ', '3E':' ', '4E':' ', '5E':' ',
         }
     fleet_battle = random.sample(board.keys(), 5)
-    for battle in fleet_battle:
-        board[battle] = '@'
+    for fleet in fleet_battle:
+        board[fleet] = '@'
     return board
-
 #Board for player
-
 def player_board():
     board = {
         '1A':' ', '2A':' ', '3A':' ', '4A':' ', '5A':' ',
@@ -25,47 +21,83 @@ def player_board():
         '1D':' ', '2D':' ', '3D':' ', '4D':' ', '5D':' ',
         '1E':' ', '2E':' ', '3E':' ', '4E':' ', '5E':' ',
         }
-
     fleet_battle = []
-    print('Where do you want to place your fleet of ships? You have 5 ships!\n')
+    print('Where do you want to place your fleet of ships ? You have 5 ships!\n')
     print('You must choose a number(1 to 5) and a letter(A to E)')
-    while len(fleet_battle) < 6:
-        battle = input()
-        if battle not in board.keys():
+    while len(fleet_battle) < 5:
+        fleet = input()
+    if fleet not in board.keys():
             print('That\'s outside the battle zone')
-        if battle in fleet_battle:
+    elif fleet in fleet_battle:
             print('This slot is already taken')
-        else: 
-            fleet_battle.append(battle)
-    for battle in fleet_battle:
-        board[battle] = '@'
+    else:             fleet_battle.append(fleet)
+    for fleet in fleet_battle:
+        board[fleet] = '@'
     return board
 
 ## function to display the board 
+
 def display_board(board):
 
-    print('---A---B---C---D---E---')
-    print('1| ' + board['1A'] + ' | ' + board['2A'] + ' | ' + board['3A'] + ' | ' + board['4A'] + ' | ' + board['5A'] + ' |')
+    print('---1---2---3---4---5---')
+    print('A| ' + board['1A'] + ' | ' + board['2A'] + ' | ' + board['3A'] + ' | ' + board['4A'] + ' | ' + board['5A'] + ' |')
     print('-+-+-+-+-+-+-+-+-+-+-+')
-    print('2| ' + board['1B'] + ' | ' + board['2B'] + ' | ' + board['3B'] + ' | ' + board['4B'] + ' | ' + board['5B'] + ' |')
+    print('B| ' + board['1B'] + ' | ' + board['2B'] + ' | ' + board['3B'] + ' | ' + board['4B'] + ' | ' + board['5B'] + ' |')
     print('-+-+-+-+-+-+-+-+-+-+-+')
-    print('3| ' + board['1C'] + ' | ' + board['2C'] + ' | ' + board['3C'] + ' | ' + board['4C'] + ' | ' + board['5C'] + ' |')
+    print('C| ' + board['1C'] + ' | ' + board['2C'] + ' | ' + board['3C'] + ' | ' + board['4C'] + ' | ' + board['5C'] + ' |')
     print('-+-+-+-+-+-+-+-+-+-+-+')
-    print('4| ' + board['1D'] + ' | ' + board['2D'] + ' | ' + board['3D'] + ' | ' + board['4D'] + ' | ' + board['5D'] + ' |')
+    print('D| ' + board['1D'] + ' | ' + board['2D'] + ' | ' + board['3D'] + ' | ' + board['4D'] + ' | ' + board['5D'] + ' |')
     print('-+-+-+-+-+-+-+-+-+-+-+')
-    print('5| ' + board['1E'] + ' | ' + board['2E'] + ' | ' + board['3E'] + ' | ' + board['4E'] + ' | ' + board['5E'] + ' |')
+    print('E| ' + board['1E'] + ' | ' + board['2E'] + ' | ' + board['3E'] + ' | ' + board['4E'] + ' | ' + board['5E'] + ' |')
     print('-+-+-+-+-+-+-+-+-+-+-+')
 
-
-# function to play the game
+    # function to play the game
 def battleship_game():
-    
     # computer's and player's boards
     ai_board = computer_board()
     user_board = player_board()
-
+    
     # display player's board
     display_board(user_board)
+
+    turn = 'Player'
+    player_fleet = 5
+    ai_fleet = 5 
+
+    while player_fleet >= 5 or ai_fleet >= 5:
+        for i in user_board:
+            if turn == 'Player':
+                print('Your Turn')
+                hit = input()
+                if hit not in user_board.keys():
+                    print('You fired outside the war-zone. Try again!')
+                elif computer_board()[hit] == '@':
+                    ai_fleet -= 1
+                    print('Great shot! Ship destroyed!')
+                    print(f'Player ships: {player_fleet} | Computer ships: {ai_fleet}')
+                else:
+                    print('You missed! ')
+                    break 
+        for i in user_board:       
+            if turn == 'Computer':
+                print('Computer Turn')
+                hit = random.sample(user_board.keys(), 1)
+                print(hit[0])
+                if user_board[hit[0]] == '@':
+                    player_fleet -= 1
+                    print('Computer destoyed your ship')
+                else:
+                    print('Computer missed your ships\n')
+                    print(f'Player ships: {player_fleet} | Computer ships; {ai_fleet}')
+                    break
+
+        if turn == 'Player':
+            turn = 'Computer'
+        else:
+            turn = 'Player' 
+
+
+
 
 
 battleship_game()
